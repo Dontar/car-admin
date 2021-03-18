c41965dd145c2aa220e582b8c45507a28dd116e7
Kaloyan Arsov <kaloyan.arsov@gmail.com>
2021-03-18 16:36:08 +0000

    server: Added cluster

--- server/src/index.ts
+++ server/src/index.ts
@@ -8,46 +8,55 @@ import { getPersons } from "./people/people";
 import { getCars } from "./cars/cars";
 import { getDB } from './connection';
 import { join } from 'path';
+import { isMaster, fork } from 'cluster';
+import { cpus } from 'os';
 
-config();
-const app = express();
-
-app.use(cors({
-    origin: process.env.CORS_ORIGIN
-})).use(morgan('common'));
-
-function catchAsyncErrors(handler: (...params: Parameters<RequestHandler>) => Promise<void>): RequestHandler {
-    return function (req, res, next) {
-        handler(req, res, next).catch(next);
+if (isMaster) {
+    for (let i = 0; i < cpus.length; i++) {
+        fork();
     }
+} else {
+
+    config();
+    const app = express();
+
+    app.use(cors({
+        origin: process.env.CORS_ORIGIN
+    })).use(morgan('common'));
+
+    function catchAsyncErrors(handler: (...params: Parameters<RequestHandler>) => Promise<void>): RequestHandler {
+        return function (req, res, next) {
+            handler(req, res, next).catch(next);
+        }
+    }
+
+    app.get('/cars', catchAsyncErrors(async (req, res) => {
+        const data = await getCars(req.query);
+        res.json(data);
+    }));
+
+    app.get('/companies', catchAsyncErrors(async (req, res) => {
+        const data = await getCompanies(req.query);
+        res.json(data);
+    }));
+
+    app.get('/people', catchAsyncErrors(async (req, res) => {
+        const data = await getPersons(req.query);
+        res.json(data);
+    }));
+
+    const port: number = Number(process.env.SERVER_PORT);
+    const host: string = process.env.SERVER_HOST!;
+
+    (async () => {
+        const db = await getDB();
+        await db.migrate({
+            migrationsPath: join(__dirname, '..', 'migrations')
+        });
+
+        const server = app.listen(port, host, () => {
+            const { address, port } = server.address() as AddressInfo;
+            console.info(`Listening on ${address}:${port}`);
+        });
+    })().catch(console.error);
 }
-
-app.get('/cars', catchAsyncErrors(async (req, res) => {
-    const data = await getCars(req.query);
-    res.json(data);
-}));
-
-app.get('/companies', catchAsyncErrors(async (req, res) => {
-    const data = await getCompanies(req.query);
-    res.json(data);
-}));
-
-app.get('/people', catchAsyncErrors(async (req, res) => {
-    const data = await getPersons(req.query);
-    res.json(data);
-}));
-
-const port: number = Number(process.env.SERVER_PORT);
-const host: string = process.env.SERVER_HOST!;
-
-(async () => {
-    const db = await getDB();
-    await db.migrate({
-        migrationsPath: join(__dirname, '..', 'migrations')
-    });
-
-    const server = app.listen(port, host, () => {
-        const { address, port } = server.address() as AddressInfo;
-        console.info(`Listening on ${address}:${port}`);
-    });
-})().catch(console.error);
